23bf8d688ada4218cce09605b8e0bb51a0c15e05
skoll <skolleclair@gmail.com>
2023-05-30 18:10:45 +0900

    Navigate to a new page and return

---
+++ LibraryManagement/lib/login/login_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'SecondRoute.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Library Management'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            child: const Text('Login Page'),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SecondRoute()),
+              );
+            },
+          ),
+        )
+    );
+  }
+}
